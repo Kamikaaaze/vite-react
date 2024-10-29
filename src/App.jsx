1690dd7bbe4c817c37bd0875614f2c23f1bf677e
aswin us <20br13512@rit.ac.in>
2024-10-29 19:11:37 +0530

    Its done

---
+++ src/App.jsx
@@ -0,0 +1,21 @@
+import { useState } from 'react'
+import reactLogo from './assets/react.svg'
+import viteLogo from '/vite.svg'
+import './App.css'
+
+function App() {
+  return (
+    <div>
+  
+
+  <h1>The Kite Runner</h1>
+  <p>Author : Khaled Hosseini</p>
+  <p>Summary</p>
+  <p>Absolute URLs in your source code will be resolved using the project root as base, so you can write code as if you are working with a normal static file server (except way more powerful!). Vite is also capable of handling dependencies that resolve to out-of-root file system locations, which makes it usable even in a monorepo-based setup.</p>
+</div>
+  );
+ 
+
+}
+
+export default App
