56c3be1ae067e1ce010e1f2e1eb43c9a9f2f9141
aswin us <20br13512@rit.ac.in>
2024-10-31 21:51:00 +0530

    qr code

--- src/App.jsx
+++ src/App.jsx
@@ -5,16 +5,9 @@ import './App.css'
 
 function App() {
   return (
-    <div className='background'>
-  
 
-  <h1 className='kite'>The Kite Runner</h1>
-  <p className='title-author'>Author : Khaled Hosseini</p>
-  <p>Summary</p>
-  <p className='summary'>Absolute URLs in your source code will be resolved using the project root as base, so you can write code as if you are working with a normal static file server (except way more powerful!). Vite is also capable of handling dependencies that resolve to out-of-root file system locations, which makes it usable even in a monorepo-based setup.</p>
-
-<img src='/images/wallpaperflare.com_wallpaper.jpg' className='space'></img>
-
+<div className='background'>
+<div className='card'></div>
 </div>
   );
  
