4b6a7bcceb1a2959af6fbf782b0c0cdee966dca3
aswin us <20br13512@rit.ac.in>
2024-11-01 20:38:22 +0530

    css practice

--- src/App.jsx
+++ src/App.jsx
@@ -7,7 +7,22 @@ function App() {
   return (
 
 <div className='background'>
-<div className='card'></div>
+  <input type='text' placeholder='SEARCH' className='searchBar'></input>
+  <button className='button'>GO</button>
+  <div className='profile'>
+    <img src='/images/wallpaperflare.com_wallpaper.jpg' className='thumbnail'></img>
+    <p className='detail'> Astranaut floating in space caught in 4k | ISS Live 2024</p>
+
+  </div>
+  
+<div className='card'>
+  <img src='/images/ar-qr-code-demo-nike.webp' class='codeImage'></img>
+</div>
+<div className='profile'>
+    <img src='/images/133516637261915553.jpg' className='thumbnail'></img>
+    <p className='detail'> Skyscraper live video from dubai caught in 4k | DUBAI 2024</p>
+
+  </div>
 </div>
   );
  
