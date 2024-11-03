27da5e3c3bf7ba80c302a35577f517f818d7da18
aswin us <20br13512@rit.ac.in>
2024-11-03 19:46:14 +0530

    form updated

--- src/components/FormTest.jsx
+++ src/components/FormTest.jsx
@@ -15,13 +15,17 @@ function FormTest(){
                 <input type="text" className="answers" style={{display:'block',width:'350px'}}></input>
                 <p className="questions">query type <span className="star">*</span> </p>
                 <div className="blocks">
-                    <input type="text" className="answers"></input>
-                <input type="text" className="answers"></input>
-                <p className="questions">Message <span className="star">*</span></p>
+                    <div className="toggle">
+                    <div style={{display:'inline-block'} }className="rectangle"><input type='radio'></input>General Enquiery</div>
+                    <div style={{display:'inline-block'}} className="rectangle"><input type='radio'></input>Support Request</div>
+                    </div>
+                  
+                    </div>
+                <p className="questions" style={{display:'block'}}>Message <span className="star">*</span></p>
                 <textarea id="message" rows='4' cols='46' style={{display:'block' , marginBottom:'20px'}}></textarea>
                 <input type="checkbox" className="checkBox" ></input>
                 <p style={{display:'inline-block'}}>I consent to be contacted by the team</p>
-                </div>
+               
                 
                 <button className="submit">Submit</button>
             </div>
