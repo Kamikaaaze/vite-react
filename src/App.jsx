0603ed37091bd1496ec559780609d60fb07d2bb5
aswin us <20br13512@rit.ac.in>
2024-11-04 16:45:52 +0530

    profile cards

--- src/App.jsx
+++ src/App.jsx
@@ -2,13 +2,14 @@ import { useState } from 'react'
 import reactLogo from './assets/react.svg'
 import viteLogo from '/vite.svg'
 import FormTest from './components/FormTest.jsx'
+import Card from './components/Card.jsx'
 import './App.css'
 
 function App() {
   return (
+<Card/>
 
-
-<FormTest/>
+// <FormTest/>
   );
  
 
---
+++ src/components/Card.jsx
@@ -0,0 +1,19 @@
+function Card(){
+    return(
+        <div className="cardBackground">
+            <div className="card">
+                <div className="one">
+                    <img src="./images/wallpaperflare.com_wallpaper.jpg" className="photo"></img>
+                </div>
+                <div className="two"><p className="learning">Learning</p>
+                <p className="publish">Published 21 Dec 2023</p>
+                <p className="html">HTML & CSS FOUNDATIONS</p>
+                <p className="def">These languages are the backbone of every website, defining structure, content, and presentation.</p></div>
+                <div className="three"><img src="\images\133516637261915553.jpg" className="dp"></img>
+                <p className="author">Greg Hooper</p></div>
+            </div>
+            
+        </div>
+    );
+}
+export default Card;
